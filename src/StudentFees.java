e096e88627cea30952d4c0fcae4f8386d014e5b8
Shubham Midgule <shubham.midgule17@gmail.com>
2026-04-09 10:39:15 +0530

    Add complete student fees management functionality
    - Implement search functionality for active (living) students
    - Auto-fill student details and current month
    - Add fees save functionality using PreparedStatement
    - Prevent duplicate fee entries for same month
    - Implement clear method to reset form and table
    - Add tableDetails method to display fee history
    - Improve validation and error handling
    - Enhance UI behavior (disable fields, hide search button if already paid)

--- src/StudentFees.java
+++ src/StudentFees.java
@@ -90,13 +90,13 @@ public class StudentFees extends javax.swing.JFrame {
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null}
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "Month", "Amount"
             }
         ));
         jScrollPane1.setViewportView(jTable1);
@@ -176,11 +176,21 @@ public class StudentFees extends javax.swing.JFrame {
         jButton3.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
         jButton3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/save.png"))); // NOI18N
         jButton3.setText("Save");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
         getContentPane().add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(311, 337, -1, -1));
 
         jButton4.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
         jButton4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/clear.png"))); // NOI18N
         jButton4.setText("Clear");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
         getContentPane().add(jButton4, new org.netbeans.lib.awtextra.AbsoluteConstraints(567, 337, -1, -1));
 
         jLabel7.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/pages background.jpg"))); // NOI18N
@@ -232,15 +242,42 @@ public class StudentFees extends javax.swing.JFrame {
             tableDetails();
             
             ResultSet rs1 = st.executeQuery(
-                "SELECT * FROM  fees inner join student where student.status='living' and fees.month='"+month+"' and fees.mobileNo='"+mobileNo+"' student.mobileNo='"+mobileNo+"'");
+                "SELECT * FROM  fees inner join student where student.status='living' and fees.month='"+month+"' and fees.mobileNo='"+mobileNo+"' and student.mobileNo='"+mobileNo+"'");
             
             if (rs1.next()) {
-                jButton2.setVisible(false);
+                JOptionPane.showMessageDialog(null, "Fees already paid by student for this month");
+                jButton3.setVisible(false);
             }
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, e);
         }
     }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+        String mobileNo = jTextField1.getText();
+        String month = jTextField5.getText();
+        String amount = jTextField6.getText();
+
+        try {
+            Connection con = ConnectionProvider.getCon();
+            PreparedStatement ps = con.prepareStatement("insert into fees values(?, ?, ?)");
+            ps.setString(1, mobileNo);
+            ps.setString(2, month);
+            ps.setString(3, amount);
+            ps.executeUpdate();
+            tableDetails();
+            JOptionPane.showMessageDialog(null, "Successfully updated");
+            clear();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+        }
+    }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        // TODO add your handling code here:
+        clear();
+    }//GEN-LAST:event_jButton4ActionPerformed
 
     /**
      * @param args the command line arguments
