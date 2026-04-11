a2a3bbf186693c6c99bcd00d8c9bfc12c551d2cd
Shubham Midgule <shubham.midgule17@gmail.com>
2026-04-11 10:45:59 +0530

    Implement Leaved Students module with database integration
    - Fetch students with status 'leaved' from database
    - Populate JTable dynamically using DefaultTableModel
    - Display complete student details in tabular format
    - Add close button functionality
    - Configure table layout and scrolling

--- src/home.java
+++ src/home.java
@@ -328,6 +328,7 @@ public class home extends javax.swing.JFrame {
         // TODO add your handling code here:        resetButtons();
         jButton6.setForeground(Color.red);
         jButton6.setBackground(new Color(255,255,255));
+        new LeavedStudents().setVisible(true);
     }//GEN-LAST:event_jButton6ActionPerformed
 
     private void jButton7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton7ActionPerformed
